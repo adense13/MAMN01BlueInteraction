dc58828959a5be6093c2132b4b2cde7a2a24619a
Adrian <adense13@gmail.com>
2016-04-29 12:36:29 +0200

    Map in GPS Activity

--- app/src/main/java/blueinteraction/mamn01blueinteraction/GPSActivity.java
+++ app/src/main/java/blueinteraction/mamn01blueinteraction/GPSActivity.java
@@ -1,5 +1,6 @@
 package blueinteraction.mamn01blueinteraction;
 
+import android.Manifest;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -34,6 +35,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -42,13 +49,14 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class GPSActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener,SensorEventListener {
+public class GPSActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, SensorEventListener {
 
     //VIEWS
-    private TextView tvHeading, direction, textAngleToLocation, latitude_check_textview, longitude_check_textview, time_elasped;
+    //private TextView tvHeading, direction, textAngleToLocation, latitude_check_textview, longitude_check_textview, time_elasped;
+    private TextView time_elasped;
     private ImageView mCompass;
-    private TextView mLatitudeTextView;
-    private TextView mLongitudeTextView;
+    //private TextView mLatitudeTextView;
+    //private TextView mLongitudeTextView;
 
     //SENSORS AND API
     private SensorManager mSensorManager;
@@ -56,6 +64,7 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
     private final int MY_PERMISSION_ACCESS_FINE_LOCATION = 1;
+    GoogleMap mMap;
 
     //COMPASS
     private double minAngle = 10;
@@ -98,6 +107,14 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
+
+        //MAPS-----------------------------------------------------------------------------------
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //END MAPS-----------------------------------------------------------------------------------
+
         initViews();
 
         //GET PUT EXTRAS--------------------------
@@ -132,28 +149,29 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
         //Timer
         time_elasped = (TextView) findViewById(R.id.time_elasped);
         //timer = new CountDownTimer(game_time *60*1000,1000){
-            timer = new CountDownTimer(30000,1000){
-            public void onTick(long millisUntilFinished){
-                time_elasped.setText("seconds remaining: "+ millisUntilFinished /1000);
+        timer = new CountDownTimer(30000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                time_elasped.setText("seconds remaining: " + millisUntilFinished / 1000);
             }
-            public void onFinish(){
+
+            public void onFinish() {
                 time_elasped.setText("Done!");
             }
         }.start();
 
     }
 
-    public void initViews(){
-        mLatitudeTextView = (TextView) findViewById((R.id.latitude_textview));
-        mLongitudeTextView = (TextView) findViewById((R.id.longitude_textview));
-        longitude_check_textview = (TextView) findViewById((R.id.longitude_check_textview));
-        latitude_check_textview = (TextView) findViewById((R.id.latitude_check_textview));
-        //time_elasped = (TextView) findViewById((R.id.time_elasped));
+    public void initViews() {
+//        mLatitudeTextView = (TextView) findViewById((R.id.latitude_textview));
+//        mLongitudeTextView = (TextView) findViewById((R.id.longitude_textview));
+//        longitude_check_textview = (TextView) findViewById((R.id.longitude_check_textview))
+//       latitude_check_textview = (TextView) findViewById((R.id.latitude_check_textview));
+        time_elasped = (TextView) findViewById((R.id.time_elasped));
 
         //COMPASS
-        textAngleToLocation = (TextView) findViewById(R.id.textAngleToLocation);
-        tvHeading = (TextView) findViewById(R.id.headingText);
-        direction = (TextView) findViewById(R.id.direction);
+//        textAngleToLocation = (TextView) findViewById(R.id.textAngleToLocation);
+//        tvHeading = (TextView) findViewById(R.id.headingText);
+//        direction = (TextView) findViewById(R.id.direction);
         mCompass = (ImageView) findViewById(R.id.compassImg);
     }
 
@@ -162,6 +180,7 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
         super.onStart();
         mGoogleApiClient.connect();
     }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -179,7 +198,7 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this, mRotation);
-        if(v != null) {
+        if (v != null) {
             v.cancel();
         }
         //mp.stop();
@@ -191,10 +210,9 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         mLocationRequest.setInterval(5000);
         mLocationRequest.setFastestInterval(3000);
-        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
-        }
-        else{
+        } else {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         }
     }
@@ -208,36 +226,36 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
     @Override
     public void onLocationChanged(Location location) {
         mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        mLatitudeTextView.setText(String.valueOf(location.getLatitude()));
-        mLongitudeTextView.setText(String.valueOf(location.getLongitude()));
+        //mLatitudeTextView.setText(String.valueOf(location.getLatitude()));
+        //mLongitudeTextView.setText(String.valueOf(location.getLongitude()));
         latitude = location.getLatitude();
         longitude = location.getLongitude();
         //ourLocation = location;
 
         ourLocation.setLatitude(location.getLatitude());
         ourLocation.setLongitude(location.getLongitude());
-        if(startLocation == null){
+        if (startLocation == null) {
             startLocation = ourLocation;
             checkpointSpawnPlayerLocation = ourLocation;
             checkpointSpawnTime = System.currentTimeMillis();
 
             //HARDCODED HIGHSCORE STUFF
-            highscoreSet.add("123"+" "+"          20/4 16:00:04");
-            highscoreSet.add("789"+" "+"          20/4 16:30:04");
-            highscoreSet.add("1234"+" "+"          20/4 16:45:45");
-            highscoreSet.add("567"+" "+"          20/4 15:54:13");
-            highscoreSet.add("456"+" "+"          20/4 17:14:12");
+            highscoreSet.add("123" + " " + "          20/4 16:00:04");
+            highscoreSet.add("789" + " " + "          20/4 16:30:04");
+            highscoreSet.add("1234" + " " + "          20/4 16:45:45");
+            highscoreSet.add("567" + " " + "          20/4 15:54:13");
+            highscoreSet.add("456" + " " + "          20/4 17:14:12");
             editor.putStringSet("Highscore", highscoreSet);
             editor.commit();
             checkpointLocation = newCheckpoint();
 
         }
-        latitude_check_textview.setText(String.valueOf(checkpointLocation.getLatitude()));
-        longitude_check_textview.setText(String.valueOf(checkpointLocation.getLongitude()));
-        Toast.makeText(this, "Distance: " + (int)ourLocation.distanceTo(checkpointLocation) +"m", Toast.LENGTH_LONG).show();//show checkpoint
+        //latitude_check_textview.setText(String.valueOf(checkpointLocation.getLatitude()));
+        //longitude_check_textview.setText(String.valueOf(checkpointLocation.getLongitude()));
+        Toast.makeText(this, "Distance: " + (int) ourLocation.distanceTo(checkpointLocation) + "m", Toast.LENGTH_LONG).show();//show checkpoint
 
         Float distance = ourLocation.distanceTo(checkpointLocation);
-        if(distance < Constants.GAME_CHECKPOINT_MINDISTANCE_METERS){
+        if (distance < Constants.GAME_CHECKPOINT_MINDISTANCE_METERS) {
             Toast.makeText(this, "Checkpoint reached", Toast.LENGTH_LONG).show();
             sendNotification("Checkpoint reached!");
             mp.stop();
@@ -295,16 +313,16 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
             float azimuthInDegress = (float) (Math.toDegrees(mOrientation2[0]) + 360) % 360; //idea: (int)
             int nbr = Math.round(azimuthInDegress);
             String str = Integer.toString(nbr);
-            direction.setText(getDirection(nbr));
-            tvHeading.setText(str + "°");
+            //direction.setText(getDirection(nbr));
+            //tvHeading.setText(str + "°");
 
-            if((ourLocation != null) && (checkpointLocation != null)) {
+            if ((ourLocation != null) && (checkpointLocation != null)) {
                 double angle = Math.abs(angelToLocation(ourLocation) - nbr);
                 if (angle > 360) {
                     angle = angle - 360;
                 }
 
-                textAngleToLocation.setText(Double.toString((int) angle));
+//                textAngleToLocation.setText(Double.toString((int) angle));
 
                 //CHECK ANGLE TO LOCATION
                 if ((angle < minAngle) || (angle > (360 - minAngle))) {
@@ -330,7 +348,7 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
         }
     }
 
-    public double angelToLocation(Location ourLocation){
+    public double angelToLocation(Location ourLocation) {
         return ourLocation.bearingTo(checkpointLocation);
     }
 
@@ -349,8 +367,8 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
     /*
      * Creates a new checkpoint and saves away the data of the old one
      */
-    public Location newCheckpoint(){
-        if(checkpointLocation != null) {
+    public Location newCheckpoint() {
+        if (checkpointLocation != null) {
             calculatePoints(checkpointSpawnPlayerLocation.distanceTo(checkpointLocation), System.currentTimeMillis() - checkpointSpawnTime); //calculate points for taking prev checkpoint (if it exists)
             //TODO: ^actually add that to the database?
         }
@@ -360,7 +378,7 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
         Random rand = new Random();
         Location l = new Location("");
 
-        while(true) { //loop for infinity, will break it from within if new checkpoint is okay
+        while (true) { //loop for infinity, will break it from within if new checkpoint is okay
             //Calculate random distance and angle from game center
             double distance = Constants.GAME_RADIUS * rand.nextDouble(); //random game_radius from start location
             double angleDeg = 360 * rand.nextDouble(); //random angle from start location
@@ -372,30 +390,30 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
             l.setLongitude(startLocation.getLongitude() + opposite);
 
             //if checkpoints spawn within permitted distance from currentLocation AND they don't spawn inside us, we break the loop. Otherwise, keep spinnin
-            if((l.distanceTo(ourLocation) <= Constants.GAME_CHECKPOINT_MAX_SPAWN_DISTANCE) && (l.distanceTo(ourLocation) > Constants.GAME_CHECKPOINT_MINDISTANCE_METERS)){
+            if ((l.distanceTo(ourLocation) <= Constants.GAME_CHECKPOINT_MAX_SPAWN_DISTANCE) && (l.distanceTo(ourLocation) > Constants.GAME_CHECKPOINT_MINDISTANCE_METERS)) {
                 break;//we are satisfied with distance between new checkpoint and current player location
             }
         }
 
         resetTimer(); // sets game_time of checkpoint spawn to current game_time
         //calculatePoints(400, timeStart);
-        if(checkpointLocation != null) { //TODO: should we even have this?
+        if (checkpointLocation != null) { //TODO: should we even have this?
             oldCheckpoints.add(checkpointLocation);
         }
         return l;
     }
 
-    public int calculatePoints(double distance, long time){
-        return (int) (10*distance/Long.valueOf(time).doubleValue()); //Points = 10*distance/game_time in seconds
+    public int calculatePoints(double distance, long time) {
+        return (int) (10 * distance / Long.valueOf(time).doubleValue()); //Points = 10*distance/game_time in seconds
     }
 
-    public void resetTimer(){
+    public void resetTimer() {
         timeStart = System.currentTimeMillis();
     }
 
-    public long getElapsedTime(){
-        long timeMillis = System.currentTimeMillis()-timeStart;
-       // time_elasped.setText(Long.toString(timeMillis/1000)+" seconds");
+    public long getElapsedTime() {
+        long timeMillis = System.currentTimeMillis() - timeStart;
+        // time_elasped.setText(Long.toString(timeMillis/1000)+" seconds");
         return timeMillis;
     }
 
@@ -404,7 +422,7 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
 
         if (timeElapsed < Constants.SOUND_TIME_LVL_2) {
             //Toast.makeText(this, "SOUND TIME LVL 1", Toast.LENGTH_SHORT).show();
-            if(!soundLvl1) {
+            if (!soundLvl1) {
                 soundLvl1 = true;
                 soundLvl2 = false;
 //                mp.setLooping(false);
@@ -413,10 +431,9 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
 //                mp.setLooping(true);
 //                mp.start();
             }
-        }
-        else if ((timeElapsed > Constants.SOUND_TIME_LVL_2)) {
+        } else if ((timeElapsed > Constants.SOUND_TIME_LVL_2)) {
             //playRandomSound(); //plays silly random noises.. but it was annoying so it's commented out :P
-            if(!soundLvl2) {
+            if (!soundLvl2) {
                 soundLvl1 = false;
                 soundLvl2 = true;
                 //THE STUFF BELOW WAS BUGGY SO IT'S COMMENTED OUT FOR NOW
@@ -436,11 +453,11 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
         //playRandomSound();
     }
 
-    public void initCheckpointLoopSound(){
+    public void initCheckpointLoopSound() {
         mp = MediaPlayer.create(this, R.raw.shortpiano1);
         mp.setLooping(true);
         mp.start();
-        mp.setVolume(0,0);
+        mp.setVolume(0, 0);
     }
 
 //    public void playRandomSound(){
@@ -471,52 +488,43 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
 //        }
 //    }
 
-    public void checkSoundEffects(){
+    public void checkSoundEffects() {
         //feedback.mediaCheck(getElapsedTime());
         mediaCheck(getElapsedTime());
     }
 
-    public String getDirection(int nbr){
-        if((nbr<30) || (nbr>=330)){
+    public String getDirection(int nbr) {
+        if ((nbr < 30) || (nbr >= 330)) {
             return ("North");
-        }
-        else if((nbr>=30) && (nbr<60)){
+        } else if ((nbr >= 30) && (nbr < 60)) {
             return ("Northeast");
-        }
-        else if((nbr>=60) && (nbr<120)){
+        } else if ((nbr >= 60) && (nbr < 120)) {
             return ("East");
-        }
-        else if((nbr>=120) && (nbr<150)){
+        } else if ((nbr >= 120) && (nbr < 150)) {
             return ("Southeast");
-        }
-        else if((nbr>=150) && (nbr<210)){
+        } else if ((nbr >= 150) && (nbr < 210)) {
             return ("South");
-        }
-        else if((nbr>=210) && (nbr<240)){
+        } else if ((nbr >= 210) && (nbr < 240)) {
             return ("Southwest");
-        }
-        else if((nbr>=240) && (nbr<300)){
+        } else if ((nbr >= 240) && (nbr < 300)) {
             return ("West");
-        }
-        else if((nbr>=300) && (nbr<330)){
+        } else if ((nbr >= 300) && (nbr < 330)) {
             return ("Northwest");
         }
         return "";
     }
 
-    public float calculateVolumePercentage(float dist){
+    public float calculateVolumePercentage(float dist) {
         float temp;
-        if(dist < 300){
-            temp = dist-Constants.GAME_CHECKPOINT_MINDISTANCE_METERS;
-            if(temp < 0){
+        if (dist < 300) {
+            temp = dist - Constants.GAME_CHECKPOINT_MINDISTANCE_METERS;
+            if (temp < 0) {
                 return 1; //full volume
-            }
-            else{
-                float volume = 1-(temp/200);
-                if(volume < 0){
+            } else {
+                float volume = 1 - (temp / 200);
+                if (volume < 0) {
                     return 0;
-                }
-                else{
+                } else {
                     return volume;
                 }
             }
@@ -569,5 +577,27 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
 
         // Issue the notification
         mNotificationManager.notify(0, builder.build());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng lund = new LatLng(55.7122, 13.20894);
+        LatLng gameStartLc = new LatLng(ourLocation.getLatitude(), ourLocation.getLongitude());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        //mMap.addMarker(new MarkerOptions().position(lund).title("Marker in Lund"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gameStartLc,17.0f));
     }
 }
